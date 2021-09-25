8be01a7c35c48ffc231e00f76e985b5771feefd7
Samuel Rutakayile <rootsum.dev@gmail.com>
2021-09-25 08:15:08 +0200

    Fix typo

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Sidebar } from './Sidebar';
 export { default as CustomInput } from './CustomInput';
 export { default as Upload } from './Upload';
-export { default as IssueDetailsViewModal } from './IssueDetailsView';
+export { default as IssueDetailsViewModal } from './IssueDetailsViewModal';
